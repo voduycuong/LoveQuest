2816d12a2bd7fcd7b455d568b5e6740bd3e25e87
duycuong <voduycuonggli@gmail.com>
2023-12-31 14:55:16 +0700

    add google authentication

---
+++ app/src/main/java/com/example/lovequest/Users.java
@@ -0,0 +1,31 @@
+package com.example.lovequest;
+
+public class Users {
+    String userID;
+    String name;
+    String profile;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
