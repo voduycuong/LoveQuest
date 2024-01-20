3c6fb837d5ccda4831a00ee780d86151c480a189
duycuong <voduycuonggli@gmail.com>
2024-01-20 23:21:29 +0700

    doing gg-calendar

--- app/src/main/java/com/example/lovequest/CalendarActivity.java
+++ app/src/main/java/com/example/lovequest/CalendarActivity.java
@@ -1,14 +1,43 @@
 package com.example.lovequest;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    Button addButton;
+    EditText addEventEditText;
+    String selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendarView = findViewById(R.id.calendarView);
+        addEventEditText = findViewById(R.id.addEventEditText);
+        addButton = findViewById(R.id.addButton);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectedDate = Integer.toString(year) + Integer.toString(month + 1) + Integer.toString(dayOfMonth);
+            }
+        });
+    }
+
+    private void calendarClicked() {
+
+    }
+
+    public void addEvent(View v) {
+
     }
 }
